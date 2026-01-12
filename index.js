39778c58b542e4307cf1286b7664ede04e3d2b52
Abdulrahman <abmmoha007@gmail.com>
2026-01-12 10:41:24 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImg = "dice" + randomNumber1 + ".png";
+var randomSource = "images/" + randomDiceImg;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImg2 = "dice" + randomNumber2 + ".png";
+var randomSource2 = "images/" + randomDiceImg2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 wins!!";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").textContent = "Player 2 wins!!";
+}
+else {
+    document.querySelector("h1").textContent = "Its a DRAW!!";
+}
